75cf9888938d153319372bcd7512d762f27276a3
cchao1031 <547160331@qq.com>
2017-06-08 23:59:46 +0800

    p4-modified1

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -92,8 +92,9 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-        if state not in self.Q:
-            self.Q[state] = {action : 0. for action in self.valid_actions}
+        if self.learning:
+            if state not in self.Q:
+                self.Q[state] = {action : 0. for action in self.valid_actions}
 
 
         return
@@ -140,7 +141,8 @@ class LearningAgent(Agent):
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
 
-        self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + self.alpha * reward
+        if self.learning:
+            self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + self.alpha * reward
 
         return
 
